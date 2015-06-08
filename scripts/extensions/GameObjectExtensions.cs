9d8b4f695af705947f3c38341e353b44939a36ff
Samuel J. Knox <evorlor@gmail.com>
2015-06-08 15:54:24 -0400

    Added GetOrAdd method to GameObjectExtensions

--- scripts/extensions/GameObjectExtensions.cs
+++ scripts/extensions/GameObjectExtensions.cs
@@ -1,8 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-using System;
 
 public static class GameObjectExtensions
 {
+
+    /// <summary>
+    /// If GameObject has specified Component, it returns it.
+    /// If GameObject does not have specified Component, it adds it and then returns it.
+    /// </summary>
+    /// <typeparam name="ComponentType">The type of Component to be (added and) retrieved</typeparam>
+    /// <param name="gameObject">The GameObject the Component check will be performed on</param>
+    /// <returns>The existing or new Component attached to the GameObject</returns>
+    public static ComponentType GetOrAddComponent<ComponentType>(this GameObject gameObject) where ComponentType : Component
+    {
+        ComponentType component = gameObject.GetComponent<ComponentType>();
+        if (component == null)
+        {
+            gameObject.AddComponent(typeof(ComponentType));
+        }
+        return gameObject.GetComponent<ComponentType>();
+    }
+
 }
